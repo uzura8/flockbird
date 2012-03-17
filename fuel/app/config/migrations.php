8896ad286d95199b2c8456ed7fda38d28637080c
shingo <uzuranoie@gmail.com>
2012-03-17 13:48:07 +0900

    add model/user.

---
+++ fuel/app/config/migrations.php
@@ -0,0 +1,18 @@
+<?php
+return array(
+	'version' => 
+	array(
+		'app' => 
+		array(
+			'default' => 1,
+		),
+		'module' => 
+		array(
+		),
+		'package' => 
+		array(
+		),
+	),
+	'folder' => 'migrations/',
+	'table' => 'migration',
+);
