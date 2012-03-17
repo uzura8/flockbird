b1df88bcd0010c8cd4d38280c9bd20f15fee5817
shingo <uzuranoie@gmail.com>
2012-03-17 18:06:02 +0900

    add comment function.

--- fuel/app/classes/controller/blog.php
+++ fuel/app/classes/controller/blog.php
@@ -16,11 +16,50 @@ class Controller_Blog extends Controller_Base
 	{
 		//$post = Model_Post::find_by_slug($slug);
 		//$post = Model_Post::find_by_slug($slug, array('related' => array('user')));
-		$post = Model_Post::find()->where('slug', $slug)->related('user')->get_one();
+		//$post = Model_Post::find()->where('slug', $slug)->related('user')->get_one();
+		$post = Model_Post::find()->where('slug', $slug)->related('user')->related('comments')->get_one();
 
 		$this->template->title = $post->title;
 		$this->template->content = View::forge('blog/view', array(
 			'post' => $post,
 		));
+	}
+
+	public function action_comment($slug)
+	{
+		$post = Model_Post::find_by_slug($slug);
+
+		// Lazy validation
+		if (Input::post('name') AND Input::post('email') AND Input::post('message'))
+		{
+			// Create a new comment
+			$post->comments[] = new Model_Comment(array(
+				'name'    => Input::post('name'),
+				'website' => Input::post('website'),
+				'email'   => Input::post('email'),
+				'message' => Input::post('message'),
+				'user_id' => $this->current_user->id,
+			));
+
+			// Save the post and the comment will save too
+			if ($post->save())
+			{
+				$comment = end($post->comments);
+				Session::set_flash('success', 'Added comment #'.$comment->id.'.');
+			}
+			else
+			{
+				Session::set_flash('error', 'Could not save comment.');
+			}
+
+			Response::redirect('blog/view/'.$slug);
+		}
+
+		// Did not have all the fields
+		else
+		{
+			// Just show the view again until they get it right
+			$this->action_view($slug);
+		}
 	}
 }
--- fuel/app/views/blog/view.php
+++ fuel/app/views/blog/view.php
@@ -7,3 +7,43 @@ by <?php echo $post->user->username ?>
 </p>
 
 <p><?php echo nl2br($post->body) ?></p>
+
+<hr />
+
+<h3 id="comments">Comments</h3>
+
+<?php foreach ($post->comments as $comment): ?>
+
+   <p><?php echo Html::anchor($comment->website, $comment->name) ?> said "<?php echo $comment->message ?>"</p>
+
+<?php endforeach; ?>
+
+<h3>Write a comment</h3>
+
+<?php echo Form::open('blog/comment/'.$post->slug) ?>
+
+<div class="row">
+   <label for="name">Name:</label>
+   <div class="input"><?php echo Form::input('name'); ?></div>
+</div>
+
+<div class="row">
+   <label for="website">Website:</label>
+   <div class="input"><?php echo Form::input('website'); ?></div>
+</div>
+
+<div class="row">
+   <label for="email">Email:</label>
+   <div class="input"><?php echo Form::input('email'); ?></div>
+</div>
+
+<div class="row">
+   <label for="message">Comment:</label>
+   <div class="input"><?php echo Form::textarea('message'); ?></div>
+</div>
+
+<div class="row">
+   <div class="input"><?php echo Form::submit('submit'); ?></div>
+</div>
+
+<?php echo Form::close() ?>
