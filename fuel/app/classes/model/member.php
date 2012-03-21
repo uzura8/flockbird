8beda7f0b862905525565433af549beba0554ad5
shingo <uzuranoie@gmail.com>
2012-03-22 03:56:22 +0900

    datetime型に対応

--- fuel/app/classes/model/member.php
+++ fuel/app/classes/model/member.php
@@ -18,11 +18,11 @@ class Model_Member extends \Orm\Model
 	protected static $_observers = array(
 		'Orm\Observer_CreatedAt' => array(
 			'events' => array('before_insert'),
-			'mysql_timestamp' => false,
+			'mysql_timestamp' => true,
 		),
 		'Orm\Observer_UpdatedAt' => array(
 			'events' => array('before_save'),
-			'mysql_timestamp' => false,
+			'mysql_timestamp' => true,
 		),
 	);
 }
