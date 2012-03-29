9caea68dcbdb55f55722f25807f3e24f8a769a5a
shingo <uzuranoie@gmail.com>
2012-03-29 21:52:38 +0900

    module名の誤りを修正

--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -199,7 +199,7 @@ return array(
 		 *
 		 * A path must be set in module_paths for this to work.
 		 */
-		'modules'  => array('mymodule'),
+		'modules'  => array('note'),
 
 		/**
 		 * Classes to autoload & initialize even when not used
