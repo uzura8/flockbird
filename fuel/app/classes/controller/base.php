ec1eeb803b8e29da3f87d0460f56386abbe10a09
shingo <uzuranoie@gmail.com>
2012-08-19 13:32:12 +0900

    Controller_Base の汎用化

--- fuel/app/classes/controller/base.php
+++ fuel/app/classes/controller/base.php
@@ -7,11 +7,6 @@ class Controller_Base extends Controller_Template
 	{
 		parent::before();
 
-		// Assign current_user to the instance so controllers can use it
-		$this->current_user = Auth::check() ? Model_Member::find()->where('id', Auth::get_member_id())->related('memberauth')->get_one() : null;
-		// Set a global variable so views can use it
-		View::set_global('current_user', $this->current_user);
-
 		// クリックジャッキング対策
 		$this->response->set_header('X-FRAME-OPTIONS', 'SAMEORIGIN');
 	}
--- fuel/app/classes/controller/site.php
+++ fuel/app/classes/controller/site.php
@@ -15,8 +15,17 @@ class Controller_Site extends Controller_Base
 	{
 		parent::before();
 
+		$this->set_current_user();
 		$this->template->header_keywords = '';
 		$this->template->header_description = '';
+	}
+
+	private function set_current_user()
+	{
+		// Assign current_user to the instance so controllers can use it
+		$this->current_user = Auth::check() ? Model_Member::find()->where('id', Auth::get_member_id())->related('memberauth')->get_one() : null;
+		// Set a global variable so views can use it
+		View::set_global('current_user', $this->current_user);
 	}
 
 	/**
