6958a6908014536462354c67dd065d5b4d689009
shingo <uzuranoie@gmail.com>
2012-10-08 22:55:43 +0900

    プロフィール写真が表示されない不具合を修正

--- fuel/app/modules/note/views/_parts/list.php
+++ fuel/app/modules/note/views/_parts/list.php
@@ -6,7 +6,7 @@
 			<h4><?php echo Html::anchor('note/detail/'.$id, $note->title); ?></h4>
 
 			<div class="member_img_box_s">
-				<?php echo img($note->member->id, $note->member->get_image(), '30x30', 'member/'.$note->member_id); ?>
+				<?php echo img($note->member->file->name, '30x30', 'member/'.$note->member_id); ?>
 				<div class="content">
 					<div class="main">
 						<b class="fullname"><?php echo Html::anchor('member/'.$note->member_id, $note->member->name); ?></b>
