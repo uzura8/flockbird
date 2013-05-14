44e944d44c74822e1ff32b2c23302380bdd86eaf
shingo <uzuranoie@gmail.com>
2013-05-14 20:46:33 +0900

    スライドページの写真がない場合の不具合を修正

--- fuel/app/modules/album/views/slide.php
+++ fuel/app/modules/album/views/slide.php
@@ -1,5 +1,6 @@
 <p><?php echo nl2br($album->body) ?></p>
 
+<?php if ($album_images): ?>
 <div class="row-fluid" id="img_comment_box">
 	<div class="span8">
 		<a name="slidetop" id="slidetop"></a>
@@ -33,3 +34,4 @@
 <?php endif; ?>
 	</div>
 </div>
+<?php endif; ?>
