cb2d7240c3fecbc0602e5f09d8a199be3b54948b
shingo <uzuranoie@gmail.com>
2013-05-17 08:08:47 +0900

    写真一覧の配置変更

--- fuel/app/modules/album/views/_parts/list_footer.php
+++ fuel/app/modules/album/views/_parts/list_footer.php
@@ -1,6 +1,32 @@
 <?php echo Asset::js('jquery.masonry.min.js');?>
 <script type="text/javascript">
-$('#article_list').masonry({
-	itemSelector : '.article'
+//$('#article_list').masonry({
+//	itemSelector : '.article'
+//});
+$(function(){
+	var $container = $('#a_container');
+	$container.imagesLoaded(function(){
+		$container.masonry({
+			itemSelector : '.a_item'
+		});
+
+		$container.infinitescroll({
+			navSelector  : '#page-nav',   // ページのナビゲーションを選択
+			nextSelector : '#page-nav a', // 次ページへのリンク
+			itemSelector : '.a_item',    // 持ってくる要素のclass
+			loading: {
+				finishedMsg: '<?php echo \Config::get('album.term.album'); ?>がありません。', //次のページがない場合に表示するテキスト
+				img: '<?php echo \Uri::create('assets/img/site/loading_l.gif'); ?>' //ローディング画像のパス
+			}
+		},
+		function( newElements ) {
+			var $newElems = $( newElements ).css({ opacity: 0 });
+			$newElems.imagesLoaded(function(){
+				$newElems.animate({ opacity: 1 });
+				$container.masonry( 'appended', $newElems, true );
+			});
+		});
+	});
 });
+
 </script>
