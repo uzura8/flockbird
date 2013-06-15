051daa1b41bcf2ae1cdf49640f9598335e28c78d
shingo <uzuranoie@gmail.com>
2013-06-16 01:20:17 +0900

    不要なアクションを削除

--- fuel/app/modules/album/classes/controller/image/comment.php
+++ fuel/app/modules/album/classes/controller/image/comment.php
@@ -49,48 +49,6 @@ class Controller_Image_comment extends \Controller_Site
 		}
 	}
 
-	public function action_create_ajax($album_image_id = null)
-	{
-		if (!$album_image_id || !$album_image = Model_AlbumImage::find($album_image_id))
-		{
-			throw new \HttpNotFoundException;
-		}
-		// Lazy validation
-		if (!\Input::post('body'))	throw new \HttpNotFoundException;
-		// if (!\Security::check_token()) throw new HttpServerErrorException;
-
-		// Create a new comment
-		$values = array(
-			'body' => \Input::post('body'),
-			'album_image_id' => $album_image_id,
-			'member_id' => $this->u->id,
-		);
-
-		$comment = new Model_AlbumImageComment($values);
-		// Save the post and the comment will save too
-		if (!$comment->save()) throw new HttpServerErrorException;
-
-		// Get the active response object
-		$response = \Request::active()->controller_instance->response;
-		if (\Input::is_ajax())
-		{
-			$response->set_header('Content-type', 'application/json');
-		}
-
-		return $response->body(json_encode(array('result' => 'success')));
-	}
-
-	public function action_list($album_image_id = null)
-	{
-		$album_image_id = (int)$album_image_id;
-		if (!$album_image_id || !$album_image = Model_Albumimage::check_authority($album_image_id))
-		{
-			return \View::forge('image/comment/list_error.php');
-		}
-
-		return \View::forge('image/comment/_parts/list.php', array('album_image' => $album_image));
-	}
-
 	/**
 	 * Album image comment delete
 	 * 
@@ -112,33 +70,5 @@ class Controller_Image_comment extends \Controller_Site
 
 		\Session::set_flash('message', \Config::get('site.term.note').'を削除しました。');
 		\Response::redirect('album/image/'.$album_image_id);
-	}
-
-	/**
-	 * Album image comment delete
-	 * 
-	 * @access  public
-	 * @params  integer
-	 * @return  Response
-	 */
-	public function action_delete_ajax($id = null)
-	{
-		$id = (int)$id;
-		if (!$id || !$album_image_comment = Model_AlbumImageComment::check_authority($id, $this->u->id))
-		{
-			throw new \HttpNotFoundException;
-		}
-		\Util_security::check_csrf(\Input::get(\Config::get('security.csrf_token_key')));
-
-		if (!$album_image_comment->delete()) throw new \HttpNotFoundException;
-
-		// Get the active response object
-		$response = \Request::active()->controller_instance->response;
-		if (\Input::is_ajax())
-		{
-			$response->set_header('Content-type', 'application/json');
-		}
-
-		return $response->body(json_encode(array('result' => 'success')));
 	}
 }
