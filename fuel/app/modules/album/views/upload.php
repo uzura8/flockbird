9544792e8fa194b3f4354ebf9a3bf9a3a1d9823a
shingo <uzuranoie@gmail.com>
2013-08-04 21:01:34 +0900

    album_image 一括アップロード画面のデザイン改修

--- fuel/app/modules/album/views/upload.php
+++ fuel/app/modules/album/views/upload.php
@@ -11,50 +11,47 @@
 			<?php echo Form::label($label, 'public_flag_'.$public_flag); ?>
 		</div>
 <?php endforeach; ?>
-<?php /*if ($val->error('public_flag')): ?>
-		<span class="help-inline error_msg"><?php echo $val->error('public_flag')->get_message(); ?></span>
-<?php endif;*/ ?>
 	</div>
 
-		<!-- The fileupload-buttonbar contains buttons to add/delete files and start/cancel the upload -->
-		<div class="row-fluid fileupload-buttonbar">
-				<div class="span8">
-						<!-- The fileinput-button span is used to style the file input field as button -->
-						<span class="btn btn-success fileinput-button">
-								<i class="icon-plus icon-white"></i>
-								<span>Add files...</span>
-								<input type="file" name="files[]" multiple>
-						</span>
-						<button type="submit" class="btn btn-primary start">
-								<i class="icon-upload icon-white"></i>
-								<span>Start upload</span>
-						</button>
-						<button type="reset" class="btn btn-warning cancel">
-								<i class="icon-ban-circle icon-white"></i>
-								<span>Cancel upload</span>
-						</button>
+	<!-- The fileupload-buttonbar contains buttons to add/delete files and start/cancel the upload -->
+	<div class="row fileupload-buttonbar">
+			<div class="span8 btn_box">
+					<!-- The fileinput-button span is used to style the file input field as button -->
+					<span class="btn btn-success fileinput-button">
+							<i class="icon-plus icon-white"></i>
+							<span>Add files...</span>
+							<input type="file" name="files[]" multiple>
+					</span>
+					<button type="submit" class="btn btn-primary start">
+							<i class="icon-upload icon-white"></i>
+							<span>Start upload</span>
+					</button>
+					<button type="reset" class="btn btn-warning cancel">
+							<i class="icon-ban-circle icon-white"></i>
+							<span>Cancel upload</span>
+					</button>
 <?php if (Config::get('album.display_setting.upload.display_delete_button', false)): ?>
-						<button type="button" class="btn btn-danger delete">
-								<i class="icon-trash icon-white"></i>
-								<span>Delete</span>
-						</button>
-						<input type="checkbox" class="toggle">
+					<button type="button" class="btn btn-danger delete">
+							<i class="icon-trash icon-white"></i>
+							<span>Delete</span>
+					</button>
+					<input type="checkbox" class="toggle">
 <?php endif; ?>
-				</div>
-				<!-- The global progress information -->
-				<div class="span4 fileupload-progress fade">
-						<!-- The global progress bar -->
-						<div class="progress progress-success progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100">
-								<div class="bar" style="width:0%;"></div>
-						</div>
-						<!-- The extended global progress information -->
-						<div class="progress-extended">&nbsp;</div>
-				</div>
-		</div>
-		<!-- The loading indicator is shown during file processing -->
-		<div class="fileupload-loading"></div>
-		<br>
-		<!-- The table listing the files available for upload/download -->
-		<table role="presentation" class="table table-striped"><tbody class="files" data-toggle="modal-gallery" data-target="#modal-gallery"></tbody></table>
+					<!-- The loading indicator is shown during file processing -->
+					<span class="fileupload-loading"></span>
+			</div>
+			<!-- The global progress information -->
+			<div class="span4 fileupload-progress fade">
+					<!-- The global progress bar -->
+					<div class="progress progress-success progress-striped active" role="progressbar" aria-valuemin="0" aria-valuemax="100">
+							<div class="bar" style="width:0%;"></div>
+					</div>
+					<!-- The extended global progress information -->
+					<div class="progress-extended">&nbsp;</div>
+			</div>
+	</div>
+
+	<!-- The table listing the files available for upload/download -->
+	<table role="presentation" class="table table-striped"><tbody class="files"></tbody></table>
 <?php echo Form::close(); ?>
 </div>
