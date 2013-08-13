2ad479fb621256fd4ea21eb0515658ba0e4d65a8
shingo <uzuranoie@gmail.com>
2013-08-14 00:05:38 +0900

    日記アップロード一時画像表示の改善

--- fuel/app/views/site/_parts/tmp_images.php
+++ fuel/app/views/site/_parts/tmp_images.php
@@ -5,18 +5,14 @@ $upload_uri = sprintf('%s%s', Config::get('site.upload.types.img.tmp.root_path.c
 <?php foreach ($file_tmps as $file_tmp): ?>
 <div class="row-fluid note_image" id="note_image_<?php echo $file_tmp->id; ?>">
 	<a class="pull-left span1 offset1"><?php echo Html::img(sprintf('%s/%s%s', $upload_uri, $file_tmp->path, $file_tmp->name), array('class' => 'img-polaroid')); ?></a>
-	<div class="span5 item">
+	<div class="span10 item">
 		<h6>Image</h6>
-		<?php echo Form::textarea('description', '', array('rows' => 1, 'placeholder' => '写真の説明', 'class' => 'span11')); ?>
-	</div>
-	<div class="span5 item">
-<?php if (Config::get('note.display_setting.form.tmp_images.image_position_radio_button')): ?>
+		<?php echo Form::textarea('description', '', array('rows' => 1, 'placeholder' => '写真の説明', 'class' => 'span5')); ?>
 		<div class="btn-group" data-toggle="buttons-radio">
 			<button type="button" class="btn">Left</button>
 			<button type="button" class="btn">Middle</button>
 			<button type="button" class="btn">Right</button>
 		</div>
-<?php endif; ?>
 		<?php echo Html::anchor('#', '<i class="ls-icon-delete"></i>', array('onclick' => 'delte_tmp_image('.$file_tmp->id.');return false;', 'class' => 'btn btn-danger delete_tmp_image', 'data-id' => $file_tmp->id)); ?>
 	</div>
 </div><!-- row-fluid -->
