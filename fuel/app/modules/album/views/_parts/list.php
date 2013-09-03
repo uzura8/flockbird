182499fe0ed78b4fbd74554fb3074a5854e761a9
shingo <uzuranoie@gmail.com>
2013-09-04 08:18:23 +0900

    アルバム編集ボタンの表示位置の不具合を修正

--- fuel/app/modules/album/views/_parts/list.php
+++ fuel/app/modules/album/views/_parts/list.php
@@ -6,14 +6,14 @@
 <div class="row-fluid">
 <div id="main_container">
 <?php foreach ($list as $album): ?>
-	<div class="main_item" id="main_item_<?php echo $album->id ?>"<?php if (!Agent::is_smartphone()): ?> onmouseover="$('#btn_album_edit_<?php echo $album->id ?>').show();" onmouseout="$('#btn_album_edit_<?php echo $album->id ?>').hide();"<?php endif; ?>>
-		<?php echo img(\Album\Site_Util::get_album_cover_filename($album->cover_album_image_id, $album->id), img_size('ai', 'M'), 'album/'.$album->id); ?>
-		<h5><?php echo Html::anchor('album/'.$album->id, strim($album->name, \Config::get('album.articles.trim_width.name'))); ?></h5>
-
+	<div class="main_item" id="main_item_<?php echo $album->id; ?>">
+		<div class="imgBox" id="imgBox_<?php echo $album->id ?>"<?php if (!IS_SP): ?> onmouseover="$('#btn_album_edit_<?php echo $album->id ?>').show();" onmouseout="$('#btn_album_edit_<?php echo $album->id ?>').hide();"<?php endif; ?>>
+			<?php echo img(\Album\Site_Util::get_album_cover_filename($album->cover_album_image_id, $album->id), img_size('ai', 'M'), 'album/'.$album->id); ?>
+			<h5><?php echo Html::anchor('album/'.$album->id, strim($album->name, \Config::get('album.articles.trim_width.name'))); ?></h5>
 <?php $disable_to_update = \Album\Site_Util::check_album_disabled_to_update($album->foreign_table); ?>
 <?php if (!empty($is_member_page)): ?>
-		<div class="date_box">
-			<small><?php echo site_get_time($album->created_at) ?></small>
+			<div class="date_box">
+				<small><?php echo site_get_time($album->created_at) ?></small>
 <?php $is_mycontents = Auth::check() && $u->id == $album->member_id; ?>
 <?php echo render('_parts/public_flag_selecter', array(
 	'model'          => 'album',
@@ -25,7 +25,7 @@
 	'have_children_public_flag' => true,
 	'child_model'    => 'album_image',
 )); ?>
-		</div>
+			</div><!-- date_box -->
 <?php else: ?>
 <?php echo render('_parts/member_contents_box', array(
 	'member'      => $album->member,
@@ -40,33 +40,34 @@
 )); ?>
 <?php endif; ?>
 
-		<div class="article">
-			<div class="body"><?php echo nl2br(strim($album->body, \Config::get('album.articles.trim_width.body'))) ?></div>
-			<small>
+			<div class="article">
+				<div class="body"><?php echo nl2br(strim($album->body, \Config::get('album.articles.trim_width.body'))) ?></div>
+				<small>
 <?php if ($album_image_count = (int)\Album\Model_AlbumImage::get_count4album_id($album->id)): ?>
 				<?php echo Html::anchor('album/slide/'.$album->id.'#slidetop', '<i class="icon-picture"></i> '.$album_image_count.' 枚'); ?>
 <?php else: ?>
 				<i class="icon-picture"></i> 0 枚
 <?php endif; ?>
-			</small>
+				</small>
 <?php if (!$disable_to_update && Auth::check() && $album->member_id == $u->id): ?>
-				<div class="btn-group btn_album_edit" id="btn_album_edit_<?php echo $album->id ?>">
+					<div class="btn-group btn_album_edit" id="btn_album_edit_<?php echo $album->id ?>">
 <?php if (\Config::get('album.display_setting.member.display_delete_link')): ?>
-					<button data-toggle="dropdown" class="btn btn-mini dropdown-toggle"><i class="ls-icon-edit"></i><span class="caret"></span></button>
-					<ul class="dropdown-menu">
-						<li><?php echo Html::anchor('album/edit/'.$album->id, '<i class="icon-pencil"></i> 編集'); ?></li>
-						<li><a href="#" onclick="delete_item('album/api/delete.json', <?php echo $album->id; ?>, '#main_item');return false;"><i class="icon-trash"></i> 削除</a></li>
-					</ul>
+						<button data-toggle="dropdown" class="btn btn-mini dropdown-toggle"><i class="ls-icon-edit"></i><span class="caret"></span></button>
+						<ul class="dropdown-menu">
+							<li><?php echo Html::anchor('album/edit/'.$album->id, '<i class="icon-pencil"></i> 編集'); ?></li>
+							<li><a href="#" onclick="delete_item('album/api/delete.json', <?php echo $album->id; ?>, '#main_item');return false;"><i class="icon-trash"></i> 削除</a></li>
+						</ul>
 <?php else: ?>
 					<?php echo Html::anchor('album/edit/'.$album->id, '<i class="ls-icon-edit mrlr10"></i>', array('class' => 'btn btn-mini')); ?>
 <?php endif; ?>
-				</div><!-- /btn-group -->
+					</div><!-- /btn-group -->
 <?php endif; ?>
-		</div>
-	</div>
+			</div><!-- article -->
+		</div><!-- img_box -->
+	</div><!-- main_item -->
 <?php endforeach; ?>
-</div>
-</div>
+</div><!-- main_container. -->
+</div><!-- row-fluid -->
 <?php endif; ?>
 
 <nav id="page-nav">
