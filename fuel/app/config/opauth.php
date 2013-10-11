a071b2fa6ba92c7abbc66ad81cb1fba4a511143a
shingo <uzuranoie@gmail.com>
2013-10-11 18:44:48 +0900

    インデントがタブになっていないファイルを修正

--- fuel/app/config/opauth.php
+++ fuel/app/config/opauth.php
@@ -18,80 +18,79 @@ namespace Opauth;
  * Copy this file to fuel/app/config and tweak as you like.
  */
 return array(
-    /**
-     * Path whre Fuel-Opauth is accessed.
-     * 
-     * Begins and ends with /.
-     * eg. if Opauth is reached via http://example.org/auth/, path is '/auth/'.
-     * if Opauth is reached via http://auth.example.org/, path is '/'.
-     */
-    'path' => '/auth/login/',
-    
-    /**
-     * Uncoment if you would like to view debug messages.
-     */
-     //'debug' => true,
-     
-     /**
-      * Callback URL: redirected to after authentication, successful or otherwise.
-      * 
-      * eg. if Opauth callback is reached via http://example.org/auth/callback, callback_url id '/auth/callback/'
-      */
-    'callback_url'  => '/auth/callback/',
-    
-    /**
-     * Callback transport, for sending of $auth response.
-     * 'session' - Default. Works best unless callback_url is on a different domain than Opauth.
-     * 'post'    - Works cross-domain, but relies on availability of client side JavaScript.
-     * 'get'     - Works cross-domain, but may be limited or corrupted by browser URL length limit
-     *             (eg. IE8/IE9 has 2083-char limit).
-     */
-     //'callback_transport' => 'session',
-     
-    /**
-     * A random string used for signing of $auth response.
-     */
-    'security_salt' => PRJ_ENCRYPTION_KEY,
-    
-    /**
-     * Higher value, better security, slower hashing.
-     * Lower value, lower security, faster hashing.
-     */
-    //'security_iteration' => 300,
-     
-    /**
-     * Time limit for valid $auth response, starting form $auth response generation to validation.
-     */
-    //'security_timeout' => '2 minutes',
-      
-    /**
-     * Directory wher Opauth strategies reside.
-     */
-    //'strategy_dir' => PKGPATH . '/Opauth/classes/Strategy/',
-    
-    /**
-     * Strategy
-     * Refer to individual strategy's documentation on configuration requirements.
-     * 
-     * eg.
-     * 'Strategy' => array(
-     *     'Facebook' => array(
-     *         'app_id' => 'APP_ID',
-     *         'app_secret' => 'APP_SECRET'
-     *     ),
-     * )
-     */
-    'Strategy' => array(
-        // Define strategies and their respective configs here
-      'Facebook' => array(
-        'app_id' => PRJ_FACEBOOK_APP_ID,
-        'app_secret' => PRJ_FACEBOOK_APP_SECRET,
-      ),
-      // https://dev.twitter.com/apps/
-      'Twitter' => array(
-        'key' => PRJ_TWITTER_APP_ID,
-        'secret' => PRJ_TWITTER_APP_SECRET,
-        'verify_credentials_json_url' => 'https://api.twitter.com/1.1/account/verify_credentials.json',
-      ),
-    ),
+	/**
+	 * Path whre Fuel-Opauth is accessed.
+	 * 
+	 * Begins and ends with /.
+	 * eg. if Opauth is reached via http://example.org/auth/, path is '/auth/'.
+	 * if Opauth is reached via http://auth.example.org/, path is '/'.
+	 */
+	'path' => '/auth/login/',
+	
+	/**
+	 * Uncoment if you would like to view debug messages.
+	 */
+	 //'debug' => true,
+	 
+	 /**
+		* Callback URL: redirected to after authentication, successful or otherwise.
+		* 
+		* eg. if Opauth callback is reached via http://example.org/auth/callback, callback_url id '/auth/callback/'
+		*/
+	'callback_url'  => '/auth/callback/',
+	
+	/**
+	 * Callback transport, for sending of $auth response.
+	 * 'session' - Default. Works best unless callback_url is on a different domain than Opauth.
+	 * 'post'    - Works cross-domain, but relies on availability of client side JavaScript.
+	 * 'get'     - Works cross-domain, but may be limited or corrupted by browser URL length limit
+	 *             (eg. IE8/IE9 has 2083-char limit).
+	 */
+	 //'callback_transport' => 'session',
+	 
+	/**
+	 * A random string used for signing of $auth response.
+	 */
+	'security_salt' => PRJ_ENCRYPTION_KEY,
+	
+	/**
+	 * Higher value, better security, slower hashing.
+	 * Lower value, lower security, faster hashing.
+	 */
+	//'security_iteration' => 300,
+	 
+	/**
+	 * Time limit for valid $auth response, starting form $auth response generation to validation.
+	 */
+	//'security_timeout' => '2 minutes',
+		
+	/**
+	 * Directory wher Opauth strategies reside.
+	 */
+	//'strategy_dir' => PKGPATH . '/Opauth/classes/Strategy/',
+	
+	/**
+	 * Strategy
+	 * Refer to individual strategy's documentation on configuration requirements.
+	 * 
+	 * eg.
+	 * 'Strategy' => array(
+	 *     'Facebook' => array(
+	 *         'app_id' => 'APP_ID',
+	 *         'app_secret' => 'APP_SECRET'
+	 *     ),
+	 * )
+	 */
+	'Strategy' => array(
+		// Define strategies and their respective configs here
+		'Facebook' => array(
+			'app_id' => PRJ_FACEBOOK_APP_ID,
+			'app_secret' => PRJ_FACEBOOK_APP_SECRET,
+		),
+		'Twitter' => array(
+			'key' => PRJ_TWITTER_APP_ID,
+			'secret' => PRJ_TWITTER_APP_SECRET,
+			'verify_credentials_json_url' => 'https://api.twitter.com/1.1/account/verify_credentials.json',
+		),
+	),
 );
