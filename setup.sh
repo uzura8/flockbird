2900df9289ecbf99117120665c51f28bc20afbdf
shingo <uzuranoie@gmail.com>
2013-10-16 22:36:06 +0900

    install 時の不具合を修正

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ cp config.php.sample config.php
 git submodule init
 git submodule foreach 'git co 1.6/master'
 git submodule update
+php composer.phar update
 php oil refine install
 chmod -R 777 public/media/*
 
