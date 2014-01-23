8c3e0a0ccf41912c8f9c9035e49a7320049b2571
shingo <uzuranoie@gmail.com>
2014-01-23 09:37:00 +0900

    不要な js 処理を削除

--- fuel/app/views/member/_parts/myhome_footer.php
+++ fuel/app/views/member/_parts/myhome_footer.php
@@ -1,5 +1,2 @@
 <?php echo Asset::js('site/common/timelines.js');?>
 <?php echo render('filetmp/_parts/upload_footer'); ?>
-<script>
-<?php echo render('_parts/script/get_public_flags'); ?>
-</script>
