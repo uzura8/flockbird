7b5db2db16ed6607dc276fa5e029f334ea0ffc5b
shingo <uzuranoie@gmail.com>
2014-02-20 08:11:00 +0900

    member_profile の form と DB 保存処理を class 化した

---
+++ fuel/app/classes/site/memberprofile.php
@@ -0,0 +1,296 @@
+<?php
+
+class Site_MemberProfile
+{
+	private $profiles = null;
+	private $member_id = 0;
+	private $member_profiles_profile_id_indexed = array();
+	private $member_profile_public_flags = array();
+	private $validation = null;
+	private $validated_values = array();
+
+	public function __construct($page_type, $member_id, $member_name = null)
+	{
+		$this->member_id = $member_id;
+		$this->profiles = Model_Profile::get4page_type($page_type);
+		$this->set_member_profiles_profile_id_indexed($member_id);
+		$this->set_member_profile_public_flags();
+		$this->set_validation($member_name);
+	}
+
+	public function set_member_profiles_profile_id_indexed($member_id = null)
+	{
+		$member_profiles = $member_id ? Model_MemberProfile::get4member_id($member_id) : array();
+		$this->member_profiles_profile_id_indexed = self::convert2member_profiles_profile_id_indexed($this->profiles, $member_profiles);
+	}
+
+	public function get_profiles()
+	{
+		return $this->profiles;
+	}
+
+	public function get_validation()
+	{
+		return $this->validation;
+	}
+
+	public function get_member_profile_public_flags()
+	{
+		return $this->member_profile_public_flags;
+	}
+
+	public function validate_public_flag($post_key = 'public_flag')
+	{
+		foreach ($this->profiles as $profile)
+		{
+			if (!$profile->is_edit_public_flag) continue;
+			$values = Input::post($post_key);
+			if (is_null($values[$profile->id]) || !in_array($values[$profile->id], Site_Util::get_public_flags()))
+			{
+				throw new HttpInvalidInputException('公開範囲の値が不正です。');
+			}
+		}
+	}
+
+	public function seve()
+	{
+		foreach ($this->profiles as $profile)
+		{
+			$profile_options = $profile->profile_option;
+			if ($profile->form_type == 'checkbox')
+			{
+				$member_profiles = (array)$this->member_profiles_profile_id_indexed[$profile->id];
+				foreach ($profile_options as $profile_option)
+				{
+					if (in_array($profile_option->id, $this->validated_values[$profile->name]))
+					{
+						$member_profile = isset($member_profiles[$profile_option->id]) ? $member_profiles[$profile_option->id] : Model_MemberProfile::forge();
+						$member_profile->member_id = $this->member_id;
+						$member_profile->profile_id = $profile->id;
+						$member_profile->profile_option_id = $profile_option->id;
+						if ($profile->is_edit_public_flag) $member_profile->public_flag = $this->member_profile_public_flags[$profile->id];
+						$member_profile->value = $profile_option->label;
+						$member_profile->save();
+					}
+					else
+					{
+						if (!isset($member_profiles[$profile_option->id])) continue;
+						$member_profiles[$profile_option->id]->delete();
+					}
+				}
+			}
+			else
+			{
+				$member_profile = $this->member_profiles_profile_id_indexed[$profile->id];
+				if (is_null($member_profile)) $member_profile = Model_MemberProfile::forge();
+				$member_profile->member_id = $this->member_id;
+				$member_profile->profile_id = $profile->id;
+				if ($profile->is_edit_public_flag) $member_profile->public_flag = $this->member_profile_public_flags[$profile->id];
+
+				if ($profile->form_type == 'radio')
+				{
+					$profile_option_id = $this->validated_values[$profile->name];
+					$member_profile->profile_option_id = $profile_option_id;
+					$member_profile->value = $profile_options[$profile_option_id]->label;
+				}
+				else
+				{
+					$member_profile->value = $this->validated_values[$profile->name];
+				}
+
+				$member_profile->save();
+			}
+		}
+
+		if ($this->validation->fieldset()->field('member_name') && $member = Model_Member::check_authority($this->member_id))
+		{
+			$member->name = $this->validated_values['member_name'];
+			$member->save();
+		}
+	}
+
+	public function set_validation($member_name = '')
+	{
+		$this->validation = \Validation::forge();
+		if ($member_name)
+		{
+			$this->validation->add(
+				'member_name',
+				term('member.name'),
+				array('type' => 'text', 'value' => $member_name),
+				array('required')	
+			);
+		}
+		foreach ($this->profiles as $profile)
+		{
+			$member_profile = $this->member_profiles_profile_id_indexed[$profile->id];
+			$rules = array();
+			if ($profile->is_required) $rules[] = 'required';
+			switch ($profile->form_type)
+			{
+				case 'input':
+				case 'textarea':
+					$type = 'text';
+					if ($profile->value_type == 'email')
+					{
+						$type = 'email';
+						$rules[] = 'valid_email';
+					}
+					elseif ($profile->value_type == 'integer')
+					{
+						$type = 'number';
+						$rules[] = array('valid_string', 'numeric');
+					}
+					elseif ($profile->value_type == 'url')
+					{
+						$type = 'url';
+						$rules[] = 'valid_url';
+					}
+					elseif ($profile->value_type == 'regexp')
+					{
+						$rules[] = array('match_pattern', $profile->value_regexp);
+					}
+					if ($profile->form_type == 'textarea') $type = 'textarea';
+
+					if ($profile->value_min)
+					{
+						$rule_name = ($profile->value_type == 'integer') ? 'numeric_min' : 'min_length';
+						$rules[] = array($rule_name, $profile->value_min);
+					}
+					if ($profile->value_max)
+					{
+						$rule_name = ($profile->value_type == 'integer') ? 'numeric_max' : 'max_length';
+						$rules[] = array($rule_name, $profile->value_max);
+					}
+
+					if ($profile->is_unique)
+					{
+						$rules[] = array('unique', 'member_profile.value', array(array('profile_id', $profile->id)));
+					}
+
+					$value = !is_null($member_profile) ? $member_profile->value : '';
+
+					$this->validation->add(
+						$profile->name,
+						$profile->caption,
+						array('type' => $type, 'value' => $value, 'placeholder' => $profile->placeholder),
+						$rules
+					);
+					break;
+
+				case 'select':
+				case 'radio':
+					$type = $profile->form_type;
+					$options = Util_Orm::conv_cols2assoc($profile->profile_option, 'id', 'label');
+					if (is_null($member_profile))
+					{
+						$options_keys = array_keys($options);
+						$value = array_shift($options_keys);
+					}
+					else
+					{
+						$value = $member_profile->profile_option_id;
+					}
+					$rules[] = array('valid_string', 'numeric');
+					$rules[] = array('in_array', array_keys($options));
+
+					$this->validation->add(
+						$profile->name,
+						$profile->caption,
+						array('type' => $type, 'value' => $value, 'options' => $options),
+						$rules
+					);
+					break;
+				case 'checkbox':
+					$type = $profile->form_type;
+					$options = Util_Orm::conv_cols2assoc($profile->profile_option, 'id', 'label');
+					$value = !is_null($member_profile) ? Util_Orm::conv_col2array($member_profile, 'profile_option_id') : array();
+					$rules[] = array('checkbox_val', $options);
+					if ($profile->is_required) $rules[] = array('checkbox_require', 1);
+
+					$this->validation->add(
+						$profile->name,
+						$profile->caption,
+						array('type' => $type, 'value' => $value, 'options' => $options),
+						$rules
+					);
+					break;
+			}
+		}
+	}
+
+	// 識別名の変更がない場合は unique を確認しない
+	public function remove_unique_restraint_for_updated_value()
+	{
+		foreach ($this->profiles as $profile)
+		{
+			if (!$profile->is_unique) continue;
+			if (!in_array($profile->form_type, array('input', 'textarea'))) continue;
+			if (!$member_profile = $this->member_profiles_profile_id_indexed[$profile->id]) continue;
+			if (trim(\Input::post($profile->name)) != $member_profile->value) continue;
+
+			$this->validation->fieldset()->field($profile->name)->delete_rule('unique');
+		}
+	}
+
+	public function validate()
+	{
+		$result = $this->validation->run();
+		$this->validated_values = $this->validation->validated();
+
+		return $result;
+	}
+
+	public function get_validation_errors()
+	{
+		return $this->validation->show_errors();
+	}
+
+	private static function convert2member_profiles_profile_id_indexed($profiles, $member_profiles)
+	{
+		$member_profiles_profile_id_indexed = array();
+		foreach ($profiles as $profile)
+		{
+			$member_profile = self::get_member_profile($member_profiles, $profile->id, $profile->form_type == 'checkbox') ?: null;
+			$member_profiles_profile_id_indexed[$profile->id] = $member_profile;
+		}
+
+		return $member_profiles_profile_id_indexed;
+	}
+
+	private function set_member_profile_public_flags()
+	{
+		$posted_public_flags = Input::post('public_flag');
+		foreach ($this->profiles as $profile)
+		{
+			if (!$profile->is_edit_public_flag) continue;
+
+			$member_profile = $this->member_profiles_profile_id_indexed[$profile->id];
+			if (is_array($member_profile))
+			{
+				$member_profile = array_shift($member_profile);
+			}
+			$public_flag = isset($member_profile->public_flag) ? $member_profile->public_flag : $profile->default_public_flag;
+			if (isset($posted_public_flags[$profile->id])) $public_flag = $posted_public_flags[$profile->id];
+
+			$this->member_profile_public_flags[$profile->id] = $public_flag;
+		}
+	}
+
+	private static function get_member_profile($member_profiles, $profile_id, $is_array = false)
+	{
+		foreach ($member_profiles as $member_profile)
+		{
+			if ($member_profile->profile_id == $profile_id)
+			{
+				if (!$is_array) return $member_profile;
+
+				if (!isset($member_profile_list)) $member_profile_list = array();
+				$member_profile_list[$member_profile->profile_option_id] = $member_profile;
+			}
+		}
+		if (isset($member_profile_list)) return $member_profile_list;
+
+		return false;
+	}
+}
