e502e86f68370dddac993552bbf2be31e1e9dbe3
shingo <uzuranoie@gmail.com>
2014-03-02 23:40:47 +0900

    submodule 更新の改善

--- setup.sh
+++ setup.sh
@@ -4,8 +4,9 @@ if [ ! -e config.php ]; then
 cp config.php.sample config.php
 fi
 git submodule init
-git submodule foreach 'git fetch;git checkout 1.7/master'
+git submodule foreach 'git checkout 1.7/master'
 git submodule update
+git submodule foreach 'git fetch;git checkout 1.7/master'
 php composer.phar update
 php oil refine install
 chmod -R 777 public/media/*
