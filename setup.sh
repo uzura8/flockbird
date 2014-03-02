76d8a705c80d074c9704acc11afbe44de203b25c
shingo <uzuranoie@gmail.com>
2014-03-02 23:18:59 +0900

    setup 方法の改善

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ if [ ! -e config.php ]; then
 cp config.php.sample config.php
 fi
 git submodule init
-git submodule foreach 'git co 1.7/master'
+git submodule foreach 'git fetch;git checkout 1.7/master'
 git submodule update
 php composer.phar update
 php oil refine install
