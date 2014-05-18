7b65bad6f875d9f2c2f9ed1bb8e2762775440c11
shingo <uzuranoie@gmail.com>
2014-05-18 12:01:10 +0900

    初期インストール時に production 環境で動作するようにした

--- fuel/app/config/production/db.php
+++ fuel/app/config/production/db.php
@@ -3,12 +3,13 @@
  * The production database settings. These get merged with the global settings.
  */
 
-return array(
-	'default' => array(
-		'connection'  => array(
-			'dsn'        => 'mysql:host=localhost;dbname=fuel_prod',
-			'username'   => 'fuel_app',
-			'password'   => 'super_secret_password',
-		),
-	),
-);
+//return array(
+//	'default' => array(
+//		'connection'  => array(
+//			'dsn'        => 'mysql:host=localhost;dbname=fuel_prod',
+//			'username'   => 'fuel_app',
+//			'password'   => 'super_secret_password',
+//		),
+//	),
+//);
+return $GLOBALS['DSN']['production'];
