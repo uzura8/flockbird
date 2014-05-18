d91b3cfaf10eff6f99acff41a8fd1ae195be833b
shingo <uzuranoie@gmail.com>
2014-05-18 11:40:56 +0900

    bird module の残りを削除

--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -288,7 +288,7 @@ return array(
 		 *
 		 * A path must be set in module_paths for this to work.
 		 */
-		'modules'  => array('admin', 'note', 'album', 'timeline', 'news', 'bird'),
+		'modules'  => array('admin', 'note', 'album', 'timeline', 'news'),
 
 		/**
 		 * Classes to autoload & initialize even when not used
