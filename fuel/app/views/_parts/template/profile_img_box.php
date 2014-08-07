da90746791c6e248bcd20c01f3cb8a09c010f32d
shingo <uzuranoie@gmail.com>
2014-08-08 08:29:09 +0900

    サイドメニューのプロフィール表示の改善

--- fuel/app/views/_parts/template/profile_img_box.php
+++ fuel/app/views/_parts/template/profile_img_box.php
@@ -4,7 +4,7 @@
 	<div class="account-group js-mini-current-user" data-screen-name="<?php echo site_get_screen_name($u); ?>">
 	<?php echo img($u->get_image(), '50x50xc', '', false, site_get_screen_name($u), true); ?>
 	<div class="main"><b class="fullname"><?php echo site_get_screen_name($u); ?></b></div>
-	<small class="metadata">プロフィールを見る</small>
+	<small class="metadata"><?php echo term('profile'); ?>をみる</small>
 	</div>
 	</div>
 	</a>
