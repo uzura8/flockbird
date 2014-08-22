82cb0afe8f668597573c30f83b89b9d8508feb64
shingo <uzuranoie@gmail.com>
2014-08-22 09:18:23 +0900

    setup スクリプトの改善

--- bin/setup/setup.sh
+++ bin/setup/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-#if [ ! -e config.php ]; then
-#cp config.php.sample config.php
-#fi
 git submodule init
 git submodule foreach 'git checkout 1.8/develop'
 git submodule update
@@ -10,6 +7,7 @@ git submodule foreach 'git fetch;git checkout 1.8/develop'
 php composer.phar update
 php oil refine install
 chmod -R 777 public/media/*
+php oil r setupdb
 php oil r createuser sample@example.com password 初期メンバー
 php oil r admin::createuser admin password admin@example.icom 100
 
