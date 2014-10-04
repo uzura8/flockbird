3249071d68080ec5e8729ad57da968f28e7abf46
shingo <uzuranoie@gmail.com>
2014-10-04 21:33:54 +0900

    csrf token 名から fuel_ を削除した

--- fuel/app/config/config.php
+++ fuel/app/config/config.php
@@ -115,7 +115,7 @@ return array(
 	 */
 	'security' => array(
 		'csrf_autoload'    => false,
-		'csrf_token_key'   => 'fuel_csrf_token',
+		'csrf_token_key'   => 'csrf_token',
 		'csrf_expiration'  => 0,
 
 		/**
