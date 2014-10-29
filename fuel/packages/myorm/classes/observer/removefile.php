d72f87a8d5341a3916f6fbbbd0e4351147a31f0c
shingo <uzuranoie@gmail.com>
2014-10-30 08:11:08 +0900

    file 削除時に DB に保存可能にした bin も削除するようにした

--- fuel/packages/myorm/classes/observer/removefile.php
+++ fuel/packages/myorm/classes/observer/removefile.php
@@ -14,6 +14,10 @@ class Observer_RemoveFile extends \Orm\Observer
 	public function before_delete(\Orm\Model $obj)
 	{
 		\Site_Upload::remove_images($obj->path, $obj->name, $this->_is_tmp);
+		if (conf('upload.isSaveDb') && $obj->file_bin_id && $file_bin = Model_FileBin::find($obj->file_bin_id))
+		{
+			$file_bin->delete();
+		}
 	}
 }
 // End of file removefile.php
