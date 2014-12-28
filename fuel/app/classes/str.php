db5304f7fd43c1be70bb86764d92185995ac660e
shingo <uzuranoie@gmail.com>
2014-12-28 23:30:26 +0900

    Str::truncate をエンティティ文字に対応させた

--- fuel/app/classes/str.php
+++ fuel/app/classes/str.php
@@ -18,46 +18,146 @@ class Str extends Fuel\Core\Str
 		$tags = array();
 		if ($is_html)
 		{
-			$offset = 0;
 			$check_length = $limit;
 
-			$single_tags = array('br', 'img', 'hr');
 			// Handle all the html tags.
 			preg_match_all('/(<[^>]+>)([^<]*)/', $string, $matches, PREG_SET_ORDER);
-			foreach ($matches as $match)
+			if (!$matches)
 			{
-				if (!$offset)
+				$offset_sc = 0;
+				preg_match_all('/&[a-z]+;/i', $string, $sc_matches, PREG_SET_ORDER);
+				foreach ($sc_matches as $sc_match)
 				{
-					$offset = static::pos($string, $match[0]);
-					if ($check_length <= $offset)
+					$pos = static::pos($string, $sc_match[0], $offset_sc);
+					$advance_pos = $pos - $offset_sc;
+					if ($check_length <= $advance_pos)
 					{
-						$cut_length = $check_length;
+						$cut_length = $offset_sc + $check_length;
+						break;
+					}
+					$check_length -= $advance_pos;
+					$offset_sc = $pos;
+					$sc_str_length = static::length($sc_match[0]);
+					if ($check_length == 1)
+					{
+						$cut_length = $offset_sc + $sc_str_length;
 						break;
 					}
 
-					$check_length -= $offset;
+					$check_length--;
+					$offset_sc += $sc_str_length;
+					$cut_length += ($sc_str_length - 1);
 				}
-
-				$tag = static::sub(strtok($match[0], " \t\n\r\0\x0B>"), 1);
-				if($tag[0] != '/' && !in_array($tag, $single_tags))
+			}
+			else
+			{
+				$single_tags = array('br', 'img', 'hr');
+				$offset = 0;
+				foreach ($matches as $match)
 				{
-					$tags[] = $tag;
-				}
-				elseif (end($tags) == static::sub($tag, 1))
-				{
-					array_pop($tags);
-				}
+					if (!$offset)
+					{
+						$offset = static::pos($string, $match[0]);
 
-				$not_tag_str_length = static::length($match[2]);
-				if ($not_tag_str_length >= $check_length)
-				{	
-					$cut_length = $offset + static::length($match[1]) + $check_length;
+						$sc_check_target_str = static::sub($string, 0, $offset);
+						preg_match_all('/&[a-z]+;/i', $sc_check_target_str, $sc_matches, PREG_SET_ORDER);
+						if ($sc_matches)
+						{
+							$offset_sc = 0;
+							foreach ($sc_matches as $sc_match)
+							{
+								$pos = static::pos($sc_check_target_str, $sc_match[0], $offset_sc);
+								$advance_pos = $pos - $offset_sc;
+								if ($check_length <= $advance_pos)
+								{
+									$cut_length = $offset_sc + $check_length;
+									break 2;
+								}
+								$check_length -= $advance_pos;
+								$offset_sc = $pos;
+								$sc_str_length = static::length($sc_match[0]);
+								if ($check_length == 1)
+								{
+									$cut_length = $offset_sc + $sc_str_length;
+									break 2;
+								}
 
-					break;
+								$check_length--;
+								$offset_sc += $sc_str_length;
+								$cut_length += ($sc_str_length - 1);
+							}
+						}
+						else
+						{
+							if ($check_length <= $offset)
+							{
+								$cut_length = $check_length;
+								break;
+							}
+							$check_length -= $offset;
+						}
+					}
+
+					// handle tag.
+					$tag = static::sub(strtok($match[0], " \t\n\r\0\x0B>"), 1);
+					if($tag[0] != '/' && !in_array($tag, $single_tags))
+					{
+						$tags[] = $tag;
+					}
+					elseif (end($tags) == static::sub($tag, 1))
+					{
+						array_pop($tags);
+					}
+
+
+					$not_tag_str_length = static::length($match[2]);
+
+					$sc_check_target_str = $match[2];
+					preg_match_all('/&[a-z]+;/i', $sc_check_target_str, $sc_matches, PREG_SET_ORDER);
+					if ($sc_matches)
+					{
+						$offset_sc = 0;
+						foreach ($sc_matches as $sc_match)
+						{
+							$pos = static::pos($sc_check_target_str, $sc_match[0], $offset_sc);
+							$advance_pos = $pos - $offset_sc;
+							if ($check_length <= $advance_pos)
+							{
+								$cut_length = $offset + static::length($match[1]) + $offset_sc + $check_length;
+								break 2;
+							}
+							$check_length -= $advance_pos;
+							$offset_sc = $pos;
+							$sc_str_length = static::length($sc_match[0]);
+							if ($check_length == 1)
+							{
+								$cut_length = $offset + static::length($match[1]) + $offset_sc + $sc_str_length;
+								break 2;
+							}
+
+							$check_length--;
+							$offset_sc += $sc_str_length;
+							$cut_length += ($sc_str_length - 1);
+						}
+						$rest_length = $not_tag_str_length - $offset_sc;
+						if ($check_length <= $rest_length)
+						{
+							$cut_length = $offset + static::length($match[1]) + $offset_sc + $check_length;
+						}
+						$check_length -= $rest_length;
+					}
+					else
+					{
+						if ($not_tag_str_length >= $check_length)
+						{	
+							$cut_length = $offset + static::length($match[1]) + $check_length;
+							break;
+						}
+						$check_length -= $not_tag_str_length;
+					}
+					$cut_length += static::length($match[1]);
+					$offset += static::length($match[0]);
 				}
-
-				$check_length -= $not_tag_str_length;
-				$offset += static::length($match[0]);
 			}
 		}
 		$new_string = static::sub($string, 0, $cut_length);
--- fuel/app/tests/str.php
+++ fuel/app/tests/str.php
@@ -28,6 +28,18 @@ class Test_Str extends TestCase
 		$expected = 'あい...';
 		$data[] = array($test, 2, '...', false, $expected);
 
+		$test     = 'あいうえお';
+		$expected = 'あい...';
+		$data[] = array($test, 2, '...', false, $expected);
+
+		$test     = 'aい<b>cd</b>efg';
+		$expected = 'aい<b>c・・・';
+		$data[] = array($test, 6, '・・・', false, $expected);
+
+		$test     = 'aい<b>cd</b>efg';
+		$expected = 'aい<b...';
+		$data[] = array($test, 4, '...', false, $expected);
+
 		$test     = 'ab<b>cd</b>efg';
 		$expected = 'ab<b>c</b>...';
 		$data[] = array($test, 3, '...', true, $expected);
@@ -56,13 +68,73 @@ class Test_Str extends TestCase
 		$expected = 'あ<b>い</b>う<a href="http://google.com/" target="_blank">えお</a>かき<br />く...';
 		$data[] = array($test, 8, '...', true, $expected);
 
-//		$test     = 'ab&amp;<b>c</b>d&lt;ef';
-//		$expected = 'ab&amp;<b>c</b>d&lt;...';
-//		$data[] = array($test, 6, '...', true, $expected);
-//
-//		$test     = 'あい&amp;えお&lt;かき';
-//		$expected = 'あい&amp;えお&lt;...';
-//		$data[] = array($test, 6, '...', true, $expected);
+		$test     = 'あい&amp;えお&lt;かき';
+		$expected = 'あ...';
+		$data[] = array($test, 1, '...', true, $expected);
+
+		$test     = 'あい&amp;えお&lt;かき';
+		$expected = 'あい&amp;...';
+		$data[] = array($test, 3, '...', true, $expected);
+
+		$test     = 'あい&amp;えお&lt;かき';
+		$expected = 'あい&amp;え...';
+		$data[] = array($test, 4, '...', true, $expected);
+
+		$test     = 'あい&amp;えお&lt;かき';
+		$expected = 'あい&amp;えお&lt;か...';
+		$data[] = array($test, 7, '...', true, $expected);
+
+		$test     = 'あい&amp;えお&lt;かき';
+		$expected = 'あい&amp;えお&lt;かき';
+		$data[] = array($test, 8, '...', true, $expected);
+
+		$test     = 'あい&amp;えお&lt;かき';
+		$expected = 'あい&amp;えお&lt;かき';
+		$data[] = array($test, 13, '...', true, $expected);
+
+		$test     = 'ab&amp;<b>c</b>d&lt;ef';
+		$expected = 'ab...';
+		$data[] = array($test, 2, '...', true, $expected);
+
+		$test     = 'ab&amp;<b>c</b>d&lt;ef';
+		$expected = 'ab&amp;...';
+		$data[] = array($test, 3, '...', true, $expected);
+
+		$test     = 'ab&amp;<b>c</b>d&lt;ef';
+		$expected = 'ab&amp;<b>c</b>...';
+		$data[] = array($test, 4, '...', true, $expected);
+
+		$test     = 'ab&amp;<b>c</b>d&lt;ef';
+		$expected = 'ab&amp;<b>c</b>d...';
+		$data[] = array($test, 5, '...', true, $expected);
+
+		$test     = 'ab&amp;<b>c</b>d&lt;ef';
+		$expected = 'ab&amp;<b>c</b>d&lt;...';
+		$data[] = array($test, 6, '...', true, $expected);
+
+		$test     = 'あ<div>&amp;<a href="http://google.com" target="_blank">&lt;え</a><hr>&amp;</div>&amp;き';
+		$expected = 'あ<div>&amp;</div>...';
+		$data[] = array($test, 2, '...', true, $expected);
+
+		$test     = 'あ<div>&amp;<a href="http://google.com" target="_blank">&lt;え</a><hr>&amp;</div>&amp;き';
+		$expected = 'あ<div>&amp;<a href="http://google.com" target="_blank">&lt;</a></div>...';
+		$data[] = array($test, 3, '...', true, $expected);
+
+		$test     = 'あ<div>&amp;<a href="http://google.com" target="_blank">&lt;え</a><hr>&amp;</div>&amp;き';
+		$expected = 'あ<div>&amp;<a href="http://google.com" target="_blank">&lt;え</a><hr>&amp;</div>...';
+		$data[] = array($test, 5, '...', true, $expected);
+
+		$test     = 'あ<div>&amp;<a href="http://google.com" target="_blank">&lt;え</a><hr>&amp;</div>&amp;き';
+		$expected = 'あ<div>&amp;<a href="http://google.com" target="_blank">&lt;え</a><hr>&amp;</div>&amp;...';
+		$data[] = array($test, 6, '...', true, $expected);
+
+		$test     = 'あ<div>&amp;<a href="http://google.com" target="_blank">&lt;え</a><hr>&amp;</div>&amp;き';
+		$expected = 'あ<div>&amp;<a href="http://google.com" target="_blank">&lt;え</a><hr>&amp;</div>&amp;き';
+		$data[] = array($test, 7, '...', true, $expected);
+
+		$test     = 'あ<div>&amp;<a href="http://google.com" target="_blank">&lt;え</a><hr>&amp;</div>&amp;き';
+		$expected = 'あ<div>&amp;<a href="http://google.com" target="_blank">&lt;え</a><hr>&amp;</div>&amp;き';
+		$data[] = array($test, 8, '...', true, $expected);
 
 		return $data;
 	}
