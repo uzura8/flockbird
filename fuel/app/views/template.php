1a0a3d9670bb074eabcd1449d783861f2dcbc239
shingo <uzuranoie@gmail.com>
2015-04-15 08:02:21 +0900

    Returned meta description of google verification

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -8,6 +8,7 @@
 <meta name="keywords" content="<?php echo site_header_keywords($header_keywords); ?>">
 <title><?php echo $header_title ? $header_title : $title; ?></title>
 <meta name="robots" content="<?php if (is_prod_env()): ?>index,follow<?php else: ?>noindex,nofollow<?php endif; ?>">
+<?php if (GOOGLE_SITE_VERIFICATION): ?>  <meta name="google-site-verification" content="<?php echo GOOGLE_SITE_VERIFICATION; ?>"><?php endif; ?>
 <?php echo render('_parts/template/site_meta_ogp', array('description' => $header_description)); ?>
 <?php echo render('_parts/template/load_common_css'); ?>
 <?php echo render('_parts/template/load_site_css'); ?>
