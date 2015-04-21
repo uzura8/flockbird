1a35a7bd5e099155c753f1e148eef8b3ec4a6bd0
shingo <uzuranoie@gmail.com>
2015-04-21 19:24:33 +0900

    Improved to simplify carousel markup by for section in site_top page

--- fuel/app/views/site/_parts/slide.php
+++ fuel/app/views/site/_parts/slide.php
@@ -6,42 +6,20 @@
 		<li data-target="#myCarousel" data-slide-to="2"></li>
 	</ol>
 	<div class="carousel-inner" role="listbox">
-		<div class="item active">
-			<?php echo Html::img('assets/img/site/sample/01.jpg'); ?>
+<?php for ($i = 0; $i < 2; $i++): ?>
+		<div class="item<?php if (!$i): ?> active<?php endif; ?>">
+			<?php echo Html::img(sprintf('assets/img/site/sample/%02d.jpg', $i + 1)); ?>
 			<div class="container">
 				<div class="carousel-caption">
 					<h1><?php echo Config::get('page.site.index.slide.title') ?: FBD_SITE_NAME; ?></h1>
 					<p><?php echo Config::get('page.site.index.slide.lead') ?: FBD_SITE_DESCRIPTION; ?></p>
 <?php if (Config::get('page.site.index.slide.isDisplayRegisterBtn') && !Auth::check()): ?>
-					<p><?php echo btn('member.registration', 'member/register/signup', '', true, '', 'primary'); ?></p>
+					<p><?php echo btn('member.registration', 'member/register/signup', '', true, '', 'primary', null, null, null, null, false); ?></p>
 <?php endif; ?>
 				</div>
 			</div>
 		</div>
-		<div class="item">
-			<?php echo Html::img('assets/img/site/sample/02.jpg'); ?>
-			<div class="container">
-				<div class="carousel-caption">
-					<h1><?php echo Config::get('page.site.index.slide.title') ?: FBD_SITE_NAME; ?></h1>
-					<p><?php echo Config::get('page.site.index.slide.lead') ?: FBD_SITE_DESCRIPTION; ?></p>
-<?php if (Config::get('page.site.index.slide.isDisplayRegisterBtn') && !Auth::check()): ?>
-					<p><?php echo btn('member.registration', 'member/register/signup', '', true, '', 'primary'); ?></p>
-<?php endif; ?>
-				</div>
-			</div>
-		</div>
-		<div class="item">
-			<?php echo Html::img('assets/img/site/sample/03.jpg'); ?>
-			<div class="container">
-				<div class="carousel-caption">
-					<h1><?php echo Config::get('page.site.index.slide.title') ?: FBD_SITE_NAME; ?></h1>
-					<p><?php echo Config::get('page.site.index.slide.lead') ?: FBD_SITE_DESCRIPTION; ?></p>
-<?php if (Config::get('page.site.index.slide.isDisplayRegisterBtn') && !Auth::check()): ?>
-					<p><?php echo btn('member.registration', 'member/register/signup', '', true, '', 'primary'); ?></p>
-<?php endif; ?>
-				</div>
-			</div>
-		</div>
+<?php endfor; ?>
 	</div>
 	<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
 		<span class="sr-only">Previous</span>
