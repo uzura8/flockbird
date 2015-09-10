9698878b37300c5b0a9639deda91c71bd6d1eb7b
S.Yamada <shingo@sy-mac-2.local>
2015-09-11 08:03:27 +0900

    Included bootstrap less files in this project

--- bin/setup/setup.sh
+++ bin/setup/setup.sh
@@ -7,9 +7,6 @@ php oil refine install
 chmod -R 777 fuel/app/media
 chmod -R 777 public/media
 chmod -R 777 public/assets/cache
-cd fuel/app/assets/less/
-ln -s ../../../vendor/twbs/bootstrap/less bootstrap
-cd ../../../../
 php oil r setupassets
 chmod -R 777 public/assets/ccss
 php oil r setupdb
