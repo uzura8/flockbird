bbd129f012f39aa9386e2ece4f4bb0e4c3633d8e
S.Yamada <shingo@sy-mac-2.local>
2015-09-19 00:13:26 +0900

    Fixed bug to occure js error at file upload page

--- fuel/app/views/filetmp/_parts/upload_footer.php
+++ fuel/app/views/filetmp/_parts/upload_footer.php
@@ -1,8 +1,8 @@
 <?php echo render('filetmp/_parts/tmpl'); ?>
 <?php
+echo Asset::js('jquery-file-upload/vendor/jquery.ui.widget.js');
+echo Asset::js('jquery-file-upload/tmpl.min.js');// The Templates plugin is included to render the upload/download listings
 Asset::js(array(
-	'jquery-file-upload/vendor/jquery.ui.widget.js',
-	'jquery-file-upload/tmpl.min.js',// The Templates plugin is included to render the upload/download listings
 	'jquery-file-upload/load-image.all.min.js',// The Load Image plugin is included for the preview images and image resizing functionality
 	'jquery-file-upload/canvas-to-blob.min.js',// The Canvas to Blob plugin is included for image resizing functionality
 	'jquery-file-upload/jquery.blueimp-gallery.min.js',// blueimp Gallery script
