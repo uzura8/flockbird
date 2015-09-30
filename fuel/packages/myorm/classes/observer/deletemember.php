c602c8a6d869294bd608313bcc9ed9cfcd7dd1a8
S.Yamada <shingo@sy-mac-2.local>
2015-09-30 21:00:50 +0900

    Fixed bug disabled to delete member with profile image

--- fuel/packages/myorm/classes/observer/deletemember.php
+++ fuel/packages/myorm/classes/observer/deletemember.php
@@ -11,7 +11,7 @@ class Observer_DeleteMember extends \Orm\Observer
 	public function before_delete(\Orm\Model $obj)
 	{
 		// delete profile image.
-		if (conf('upload.types.img.types.m.save_as_album_image') && $obj->file_name && $file = Model_File::get4name($obj->file_name))
+		if (conf('upload.types.img.types.m.save_as_album_image') && $obj->file_name && $file = \Model_File::get4name($obj->file_name))
 		{
 			$file->delete();
 		}
