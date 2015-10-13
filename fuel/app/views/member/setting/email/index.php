96620549f0a8c020a08c81a75535cf71b463b1e3
S.Yamada <shingo@sy-mac-2.local>
2015-10-14 08:34:00 +0900

    Reorganized setting email directory and files

---
+++ fuel/app/views/member/setting/email/index.php
@@ -0,0 +1,15 @@
+<?php
+$label_size = 3;
+$btn_label = sprintf('%s用%sを%sする', term('form.confirm'), term('site.mail'), term('form.send'));
+?>
+<div class="well">
+<?php echo form_open(true); ?>
+<?php echo form_input($val, 'email', '', 7, $label_size); ?>
+<?php echo form_input($val, 'email_confirm', '', 7, $label_size); ?>
+<?php echo form_button($btn_label, 'submit', null, null, $label_size, null, 'form.do_send'); ?>
+<?php echo form_close(); ?>
+</div><!-- well -->
+
+<?php if (!IS_ADMIN): ?>
+<?php echo render('member/setting/_parts/footer_navi'); ?>
+<?php endif; ?>
