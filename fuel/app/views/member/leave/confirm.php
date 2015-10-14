68f982ce313fe9cafe25922ae5d8f3e3faac32f7
S.Yamada <shingo@sy-mac-2.local>
2015-10-14 20:00:39 +0900

    refs #4: Imploved to disable to delete registration for OAuh member

--- fuel/app/views/member/leave/confirm.php
+++ fuel/app/views/member/leave/confirm.php
@@ -1,13 +1,22 @@
 <?php echo alert(sprintf('本当に%sしますか？', term('site.left')), 'danger'); ?>
 <div class="well">
-<?php echo form_open(false, false, array('action' => 'member/leave')); ?>
-<?php echo Form::hidden('password', $input['password'], array('dont_prep' => true)); ?>
-	<?php echo form_button('form.back', 'submit', 'submit_back', array('class' => 'btn btn-default')); ?>
+<?php echo form_open(false, false, array('action' => 'member/leave/delete')); ?>
+	<?php echo form_button('site.leave', 'submit', 'submit', array('class' => 'btn btn-default btn-danger')); ?>
+<?php echo Form::hidden(Config::get('security.csrf_token_key'), Util_security::get_csrf()); ?>
+<?php if ($u->check_registered_oauth(true)): ?>
+	<?php echo form_button('form.back', 'button', 'submit_back', array(
+		'class' => 'btn btn-default js-simpleLink',
+		'data-uri' => 'member/setting',
+	)); ?>
+<?php else: ?>
+	<?php echo Form::hidden('password', $input['password'], array('dont_prep' => true)); ?>
+<?php endif; ?>
 <?php echo form_close(); ?>
 
-<?php echo form_open(false, false, array('action' => 'member/leave/delete')); ?>
-<?php echo Form::hidden(Config::get('security.csrf_token_key'), Util_security::get_csrf()); ?>
-<?php echo Form::hidden('password', $input['password'], array('dont_prep' => true)); ?>
-	<?php echo form_button('site.leave', 'submit', 'submit', array('class' => 'btn btn-default btn-danger')); ?>
+<?php if (!$u->check_registered_oauth(true)): ?>
+<?php echo form_open(false, false, array('action' => 'member/leave')); ?>
+	<?php echo Form::hidden('password', $input['password'], array('dont_prep' => true)); ?>
+	<?php echo form_button('form.back', 'submit', 'submit_back', array('class' => 'btn btn-default')); ?>
 <?php echo form_close(); ?>
+<?php endif; ?>
 </div>
