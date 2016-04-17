db96dcc7aea7c3ddf9b276f589c447289b0b93af
Shingo Yamada <uzuranoie@gmail.com>
2016-04-17 21:57:09 +0900

    Imploved link to member search to hide at unauth

--- fuel/app/views/member/_parts/list_subtitle.php
+++ fuel/app/views/member/_parts/list_subtitle.php
@@ -1,2 +1,4 @@
+<?php if (Auth::check()): ?>
 <?php echo btn(null, 'member/search?form_open=1', 'edit', true, null, null, null, null, null, null, false, false, icon('form.search').' '.term('site.detail', 'form.search')); ?>
+<?php endif; ?>
 
