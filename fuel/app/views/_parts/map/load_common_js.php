d6ad6893f7ac1587906afffeea18eb5ea8e9d293
Shingo Yamada <uzuranoie@gmail.com>
2016-05-25 20:37:17 +0900

    Imploved to load gmap.js for using minified file

--- fuel/app/views/_parts/map/load_common_js.php
+++ fuel/app/views/_parts/map/load_common_js.php
@@ -1,5 +1,5 @@
 <script src="//maps.google.com/maps/api/js?sensor=true"></script>
-<script src="//raw.githubusercontent.com/HPNeo/gmaps/master/gmaps.js"></script>
+<script src="//raw.githubusercontent.com/HPNeo/gmaps/master/gmaps.min.js"></script>
 <?php echo Asset::js('site/common/map.js');?>
 <?php if (!empty($is_load_template)): ?>
 <?php echo render('_parts/map/handlebars_template/marker_image'); ?>
