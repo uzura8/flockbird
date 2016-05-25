e84343fa10885d1184832010a3d2831325dde20c
Shingo Yamada <uzuranoie@gmail.com>
2016-05-25 21:02:24 +0900

    Updated jquery-ui to version 1.11.4

--- fuel/app/views/_parts/sortable_footer.php
+++ fuel/app/views/_parts/sortable_footer.php
@@ -1,6 +1,6 @@
 <?php
 Asset::js(array(
-	'jquery-ui-1.10.3.custom.js',
+	'jquery-ui.js',
 	'util/jquery-ui.js',
 ), null, 'js_jquery_ui', false, true);
 echo Asset::render('js_jquery_ui', false, 'js');
