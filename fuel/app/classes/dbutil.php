e4350ada1741b399d577f816e76a1a843ee58e5d
Shingo Yamada <uzuranoie@gmail.com>
2016-08-31 21:02:13 +0900

    Fixed bug to disabld to setup db

--- fuel/app/classes/dbutil.php
+++ fuel/app/classes/dbutil.php
@@ -11,10 +11,10 @@ class DBUtil extends Fuel\Core\DBUtil
 {
 	public static function shell_exec_create_database($database, $charset = null, $if_not_exists = true, $db = null)
 	{
-		$charset = static::process_charset($charset, true);
+		$charset = self::process_charset($charset, true);
 		$sql  = 'CREATE DATABASE';
 		$sql .= $if_not_exists ? ' IF NOT EXISTS ' : ' ';
-		$sql .= DB::quote_identifier($database, static::$connection).$charset;
+		$sql .= DB::quote_identifier($database, static::$connection);
 		if ($charset) $sql .= ' '.$charset;
 
 		return static::shell_exec_sql($sql);
@@ -49,5 +49,41 @@ class DBUtil extends Fuel\Core\DBUtil
 		}
 
 		return true;
+	}
+
+	protected static function process_charset($charset = null, $is_default = false, $collation = null)
+	{
+		$charset or $charset = Util_Db::get_conection_configs('charset');
+
+		if (empty($charset))
+		{
+			return '';
+		}
+
+		$collation or $collation = Util_Db::get_conection_configs('collation');
+
+		if (empty($collation) and ($pos = stripos($charset, '_')) !== false)
+		{
+			$collation = $charset;
+			$charset = substr($charset, 0, $pos);
+		}
+
+		$charset = 'CHARACTER SET '.$charset;
+
+		if ($is_default)
+		{
+			$charset = 'DEFAULT '.$charset;
+		}
+
+		if ( ! empty($collation))
+		{
+			if ($is_default)
+			{
+				$charset .= ' DEFAULT';
+			}
+			$charset .= ' COLLATE '.$collation;
+		}
+
+		return $charset;
 	}
 }
