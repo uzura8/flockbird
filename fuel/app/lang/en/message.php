b97d5783f5a4b9d2e9d6502a5c51f98733ce9691
Shingo Yamada <uzuranoie@gmail.com>
2016-09-11 17:17:32 +0900

    refs GL#1: Localized for not published message

--- fuel/app/lang/en/message.php
+++ fuel/app/lang/en/message.php
@@ -9,6 +9,7 @@ return array(
 	'message_please_input_following_items' => 'Please input following items. ',
 	'message_send_mail' => 'Sent :label mail. ',
 	'message_delete_confirm_for' => 'Do you delete this :label ?',
+	'message_not_published' => 'This :label is not published. ',
 	// member registration
 	'message_confirmation_to_agree_terms_of_use' => 'Please read :link . If you agree, push "Agree and register" button and complete registration. ',
 	// invite friend
