b409ad571a8d999db08cc235b7afc8a29257f790
Shingo Yamada <uzuranoie@gmail.com>
2016-10-06 21:51:08 +0900

    refs GL#1: Added message for input on lang

--- fuel/app/lang/en/message.php
+++ fuel/app/lang/en/message.php
@@ -20,6 +20,7 @@ return array(
 	'message_disabled_to_delete_for' => 'This :label is disabled to delete. ',
 	'message_not_published' => 'This :label is not published. ',
 	'message_not_registered_for' => ':label is not registered. ',
+	'message_not_input_for' => ':label is not input. ',
 	// member registration
 	'message_confirmation_to_agree_terms_of_use' => 'Please read :link . If you agree, push "Agree and register" button and complete registration. ',
 	// invite friend
