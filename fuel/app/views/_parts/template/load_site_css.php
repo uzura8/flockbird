6d292c33eab27182146325c0d22426b7c15c17f9
Shingo Yamada <uzuranoie@gmail.com>
2016-10-08 14:37:19 +0900

    Fixed bug loaded css file duplicately

--- fuel/app/views/_parts/template/load_site_css.php
+++ fuel/app/views/_parts/template/load_site_css.php
@@ -1,4 +1,3 @@
-<?php echo Asset::css('site.css', array(), null, false, false, false, true);?>
 <?php
 Asset::css('site.css', null, 'css_site', false, true, false, true);
 echo Asset::render('css_site', false, 'css');
