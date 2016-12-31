f9df809119722accc394a58cce423acadebdfa09
Shingo Yamada <uzuranoie@gmail.com>
2016-12-31 15:03:17 +0900

    Implemented link to edit page on profile address section

--- fuel/app/views/member/profile/index.php
+++ fuel/app/views/member/profile/index.php
@@ -11,7 +11,12 @@
 	&& conf('address.isDisplay', 'member')
 	&& $member_address = Model_MemberAddress::get_one_main($member->id)): ?>
 
-<h3><?php echo t('member.address.view'); ?></h3>
+<h3>
+	<?php echo t('member.address.view'); ?>
+<?php if ($member->id == get_uid()): ?>
+	<?php echo btn('form.edit', 'member/setting/address', 'edit', true, 'sm', null, array('class' => 'pull-right')); ?>
+<?php endif; ?>
+</h3>
 <?php echo render('member/_parts/address', array(
 		'member' => $member,
 		'member_address' => $member_address,
