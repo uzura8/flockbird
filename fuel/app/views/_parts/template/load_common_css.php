bb0045264da35ba99a19e5c142a7bd5c253e2a2a
Shingo Yamada <uzuranoie@gmail.com>
2017-03-05 23:33:39 +0900

    Update HTML5 Shiv to v3.7.3

--- fuel/app/views/_parts/template/load_common_css.php
+++ fuel/app/views/_parts/template/load_common_css.php
@@ -1,6 +1,6 @@
 <?php if (conf('legacyBrowserSupport')): ?>
 <!--[if lt IE 9]>
-	<script src="//oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="//oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 	<script src="//oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <![endif]-->
 <?php endif; ?>
